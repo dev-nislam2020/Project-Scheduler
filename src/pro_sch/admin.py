8f1f1bcca765a8bae6c11ba9e2a9f06dfc914c87
dev-nislam2020 <dev.nislam2020@gmail.com>
2021-03-20 11:55:04 +0600

    create superuser and migrate model project

--- src/pro_sch/admin.py
+++ src/pro_sch/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from pro_sch.models import Project
+
 # Register your models here.
+admin.site.register(Project)
--- src/pro_sch/models.py
+++ src/pro_sch/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+APP_TYPE = (
+    ('Web', 'Web App Development'),
+    ('Desktop', 'Desktop App Development'),
+    ('Mobile', 'Mobile App Development'),
+    ('Native', 'Native App Development'),
+)
+class Project(models.Model):
+    name = models.CharField(verbose_name="Project Name", max_length=100, unique=True)
+    deadline = models.DateField(verbose_name="Project Deadline")
+    app_type = models.CharField(verbose_name="Development", max_length=20, choices=APP_TYPE)
+    is_completed = models.BooleanField(default=False)
+    is_requirement_completed = models.BooleanField(default=False)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['-create_at']
+    
+    def __str__(self):
+        return self.name
