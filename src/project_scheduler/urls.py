eea8b1161eaeee12eb9ec9e9caf3c722d4483260
dev-nislam2020 <dev.nislam2020@gmail.com>
2021-03-20 10:48:04 +0600

    App urls add now

--- src/project_scheduler/urls.py
+++ src/project_scheduler/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from pro_sch.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('project-scheduler/', include('pro_sch.urls')),
 ]
